5f4ce600fa015df49a97651cd1d8d8853c5fe339
Anyrevlis <105887989+Anyrevlis@users.noreply.github.com>
2023-12-29 12:20:40 +0100

    Updated fundamentals

--- 00-Fundamentals/fundamentals.py
+++ 00-Fundamentals/fundamentals.py
@@ -6,6 +6,18 @@ x = torch.rand(5, 3)
 #print(x)
 
 # Scalar
-scalar = torch.tensor(7)
-print(scalar)
-print(scalar.ndim)
+scalar = torch.tensor(7) # 7 is a scalar with tensor type and rank 0
+#print(scalar)
+#print(scalar.ndim) # prints the dimension of the tensor
+
+vector = torch.tensor([1, 2, 3, 4, 5]) # 1D tensor
+#print(vector.ndim)
+#print(vector.shape)
+
+MATRIX = torch.tensor([[1, 2, 3], [4, 5, 6]]) # 2D tensor, two square brackets
+#print(MATRIX.ndim)
+print(MATRIX[0]) # prints the first row, index starts from 0
+
+# 3D tensor and higher
+TENSOR_3D = torch.tensor([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+
